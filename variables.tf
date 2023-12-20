5811c3368459a5461b750fe647d7a7a93fac43c4
Igor Rossetti <igor.rossetti@cloudmotion.com.br>
2023-12-19 21:12:11 -0300

    Add pointer app files

--- variables.tf
+++ variables.tf
@@ -2,9 +2,11 @@
 variable vpc_id {
   type        = string
   description = "Informe sua VPC ID"
+  default = "vpc-04efa20c0d33f37ae"
 }
 
 variable subnet_id {
   type        = string
   description = "Informe sua subnet id"
+  default = "subnet-0a422665e7f6447b6"
 }
