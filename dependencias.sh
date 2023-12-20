68308698e2ab60544b83ee8574671181d6723a97
Igor Rossetti <igor.rossetti@cloudmotion.com.br>
2023-12-19 21:33:38 -0300

    Fix: dependencias.sh

--- dependencias.sh
+++ dependencias.sh
@@ -2,7 +2,7 @@
 sudo apt-get update 
 sudo apt-get install git ansible -y
 
-git clone https://github.com/uid1root/pointer-apps.git /tmp/pointer-apps/
+git clone https://github.com/ooichir0oo/pointer-apps.git /tmp/pointer-apps/
 
 cd /tmp/pointer-apps/
 ansible-playbook pointer-playbook.yml
